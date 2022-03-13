7c42b8ecc34496ee5101a3f6d1fb6c4a1f320b58
alhabib911 <abdullahalhabib100@gmail.com>
2022-03-13 22:56:04 +0600

    question ans done

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const reportedPostsId = [];
 
 const getLikedPosts = () => {
     return posts.filter((post) => likedPostsId.includes(post.id));
+    
 };
 
 const getReportedPosts = () => {
@@ -27,7 +28,8 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-    return text.length < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+    return text.length < 30 ? text : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+    
 };
 
 const switchTab = (id) => {
@@ -121,9 +123,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${posts.comments?.user}
+                      ${post.comments?.[0]?.["user"]}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments?.[0]?.["text"]}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
